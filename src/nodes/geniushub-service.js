f8dda6d6ea5adde372c0fe71e0a6328f3d011f60
Jason Steele <jasonbsteele@live.co.uk>
2020-04-17 18:39:41 +0100

    Get Zones working

--- src/nodes/geniushub-service.js
+++ src/nodes/geniushub-service.js
@@ -1,10 +1,16 @@
+'use strict';
+
 module.exports = function(RED) {
+
+    const GeniusHubClient = require("../geniushub-client.js");
+    
     function GeniusHubServiceNode(config) {
         RED.nodes.createNode(this, config);
-        this.host = "https://my.geniushub.co.uk/v1";
+        //this.host = "https://my.geniushub.co.uk/v1";
 
-        this.token = config.token;
-
+        //this.token = config.token;
+        
+        this.geniusHubClient = new GeniusHubClient(this.credentials.token);
         //Not required 
         //this.secureApiKeyHeaderOrQueryName = n.secureApiKeyHeaderOrQueryName;
         //this.secureApiKeyIsQuery = n.secureApiKeyIsQuery;
--- src/nodes/get-zones.js
+++ src/nodes/get-zones.js
@@ -1,11 +1,20 @@
 module.exports = function(RED) {
     function GetZones(config) {
         RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+        this.service = RED.nodes.getNode(config.service);
+
+        //var node = this;
+
+        this.on('input', async msg => {
+            msg.payload = await this.service.geniusHubClient.getZones();
+                //.then(
+                //    z => z)
+                //.catch(e => console.error("Critical failure: " + e.message));
+
+            //this.payload = msg.payload.toLowerCase();
+            this.send(msg);
+            this.done();
         });
     }
-    RED.nodes.registerType("get-zones",GetZones);
+    RED.nodes.registerType("get-zones", GetZones);
 }
