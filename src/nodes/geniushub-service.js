5147f9a42f95835aa6e26e490e7f1842c50f6356
Jason Steele <jasonbsteele@live.co.uk>
2020-05-04 20:53:20 +0100

    sucessfully reading zones into drop down

--- src/nodes/geniushub-service.js
+++ src/nodes/geniushub-service.js
@@ -7,12 +7,26 @@ module.exports = function(RED) {
     function GeniusHubServiceNode(config) {
         RED.nodes.createNode(this, config);
 
-        this.geniusHubClient = new GeniusHubClient(this.credentials.token);
+        this.geniusHubClient = new GeniusHubClient(this.token);
     }
 
-    RED.nodes.registerType('geniushub2-service', GeniusHubServiceNode, {
-        credentials: {
-            token: { type: 'text' }
-        }
+    RED.nodes.registerType('geniushub2-service', GeniusHubServiceNode); //, {
+        // credentials: {
+        //     token: { type: 'text' }
+        // }
+    //});
+
+    RED.httpAdmin.get('/geniushub/zones', function(req, res, next)
+	{
+        let client = new GeniusHubClient(req.query.token);
+
+        client.getZonesSummary()
+        .then(zones  => {
+			res.end(JSON.stringify(zones));
+		})
+		.catch(error => {
+			res.send(500).send(error.message);
+		});
+
     });
 }
