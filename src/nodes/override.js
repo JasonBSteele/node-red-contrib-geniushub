0b869b2b60f0a532e3a3dbabddcfbf138ce644c8
Jason Steele <jasonbsteele@live.co.uk>
2020-05-06 21:49:08 +0100

    Pass zone type back for validation

--- src/nodes/override.js
+++ src/nodes/override.js
@@ -8,33 +8,46 @@ module.exports = function(RED) {
             RED.nodes.createNode(this,config);
             this.service = RED.nodes.getNode(config.service);
 
-            this.on('input', async (msg, send, done) => {
-                try {
-                    let payload = msg.payload;
-                    let validationMsg = this.validatePayload(payload);
-                    if (validationMsg.length > 0) {
-                        done(validationMsg);
+            let parts = config.zone.split('|');
+
+            if (parts.length == 3) {
+                let zoneId = parts[0];
+                let zoneName = parts[1];
+                let zoneType = parts[2];
+
+                this.on('input', async (msg, send, done) => {
+                    try {
+                        let payload = msg.payload;
+                        let validationMsg = this.validatePayload(zoneType, payload);
+                        if (validationMsg.length > 0) {
+                            done(validationMsg);
+                        }
+                        else {
+                            await this.service.geniusHubClient.override(zoneId, payload.setpoint, payload.duration);
+                            done();
+                        }
                     }
-                    else {
-                        await this.service.geniusHubClient.override(config.zone, payload.setpoint, payload.duration);
-                        done();
+                    catch(e) {
+                        done(e);
                     }
-                }
-                catch(e) {
-                    done(e);
-                }
-            });
+
+                });
+            }
         }
 
-        validatePayload(payload) {
+        validatePayload(zoneType, payload) {
             let validationMsg = "";
 
             // if (!Number.isInteger(payload.zoneId)) {
             //     validationMsg += `zoneId is not an integer: ${payload.zoneId}\n`; 
             // }
+            
 
             if (isNaN(payload.setpoint)) {
                 validationMsg += `setpoint is not an number: ${payload.setpoint}\n`; 
+            }
+            else if (zoneType == "on / off"){
+                validationMsg += `setpoint must be a 0 or 1 for on/off zones: ${payload.setpoint}\n`;
             }
 
             if (!Number.isInteger(payload.duration)) {
